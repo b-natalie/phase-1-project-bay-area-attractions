3d9a039b5194dfdc6f94bf935144afc90c2c2167
Natalie Barba <nbarba2010@gmail.com>
2021-08-25 09:58:31 -0700

    Added POST and PATCH, some sort functionality

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ function init() {
     .then(resp => resp.json())
     .then(data => {
         const gemContainer = document.querySelector("#gem-container");
+        const gemArray = [];
         data.forEach(attraction => {
-            // console.log(attraction, gemContainer);
-            renderGemCard(attraction, gemContainer);
+            // renderGemCard(attraction, gemContainer);
+            gemArray.push(attraction);
         });
+        sortAttractions(gemArray);
+        gemArray.forEach(attraction => renderGemCard(attraction, gemContainer));
+        addGem(gemContainer);
     })
 }
 
 function renderGemCard(gem, gemContainer) {
-
-    // console.log(gem, gemContainer);
 
     const gemCard = document.createElement("div");
     gemCard.id = `gem-${gem.id}`;
@@ -30,25 +32,172 @@ function renderGemCard(gem, gemContainer) {
     heartButton.className = "heart-button";
     heartButton.textContent = "💙";
     heartButton.addEventListener("click", event => {
-        gem.likes += 1;
-        heartsNum.textContent = gem.likes;
-    });
+        increaseLike(gem, heartsNum);
+    })
 
     const heartsNum = document.createElement("h5");
     heartsNum.className = "heart-num";
     heartsNum.textContent = gem.likes;
 
-    gemCard.append(gemImg, gemName, heartButton, heartsNum);
-    gemContainer.appendChild(gemCard);
-
     const overlayDiv = document.createElement("div");
-    overlayDiv.textContent = gem.activities[0];
     overlayDiv.className = "overlay-info"
 
-    gemCard.addEventListener("mouseover", event => {
-        console.log("Mouse over!");
-        gemCard.appendChild(overlayDiv);
+    const activity = document.createElement("p");
+    activity.textContent = gem.activities[0];
+    console.log(activity);
+
+    gemContainer.appendChild(gemCard);
+    gemCard.append(gemImg, overlayDiv);
+    overlayDiv.append(gemName, heartButton, heartsNum, activity);
+
+}
+
+function addGem(gemContainer) {
+    const addGemForm = document.querySelector("#add-gem-form");
+
+    addGemForm.addEventListener("submit", event => {
+        event.preventDefault();
+
+        let name = addGemForm.querySelector("#name-input").value;
+        let location = addGemForm.querySelector("#location-input").value;
+        let image = addGemForm.querySelector("#img-input").value;
+        let userActivity = addGemForm.querySelector("#activity-input").value;
+        let activities = [userActivity];
+
+        let gem = {
+            name,
+            location,
+            image,
+            activities,
+            likes: 0
+        }
+        renderGemCard(gem, gemContainer);
+
+        fetch("http://localhost:3000/attractions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(gem)
+        })
+    })
+}
+
+function sortAttractions(gemArray) {
+    gemArray.sort((gemA, gemB) => (gemA.likes > gemB.likes) ? -1 : 1)
+}
+
+function increaseLike(gem, heartsNum) {
+    gem.likes += 1;
+    heartsNum.textContent = gem.likes;
+
+    fetch(`http://localhost:3000/attractions/${gem.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({likes: gem.likes})
     })
 }
 
 init()
+
+// function init() {
+//     fetch("http://localhost:3000/attractions")
+//     .then(resp => resp.json())
+//     .then(data => {
+//         const gemContainer = document.querySelector("#gem-container");
+//         data.forEach(attraction => {
+//             renderGemCard(attraction, gemContainer);
+//         });
+//         addGem(gemContainer);
+//     })
+// }
+
+// function renderGemCard(gem, gemContainer) {
+
+//     const gemCard = document.createElement("div");
+//     gemCard.id = `gem-${gem.id}`;
+//     gemCard.className = "gem-card";
+
+//     const gemImg = document.createElement("img");
+//     gemImg.src = gem.image;
+//     gemImg.alt = `${gem.name} image`;
+//     gemImg.className = "gem-img"
+
+//     const gemName = document.createElement("h3");
+//     gemName.textContent = gem.name;
+
+//     const heartButton = document.createElement("button");
+//     heartButton.className = "heart-button";
+//     heartButton.textContent = "💙";
+//     heartButton.addEventListener("click", event => {
+//         increaseLike(gem, heartsNum);
+//     })
+
+//     const heartsNum = document.createElement("h5");
+//     heartsNum.className = "heart-num";
+//     heartsNum.textContent = gem.likes;
+
+//     const overlayDiv = document.createElement("div");
+//     overlayDiv.className = "overlay-info"
+
+//     const activity = document.createElement("p");
+//     activity.textContent = gem.activities[0];
+//     console.log(activity);
+
+//     gemContainer.appendChild(gemCard);
+//     gemCard.append(gemImg, overlayDiv);
+//     overlayDiv.append(gemName, heartButton, heartsNum, activity);
+
+// }
+
+// function addGem(gemContainer) {
+//     const addGemForm = document.querySelector("#add-gem-form");
+
+//     addGemForm.addEventListener("submit", event => {
+//         event.preventDefault();
+
+//         let name = addGemForm.querySelector("#name-input").value;
+//         let location = addGemForm.querySelector("#location-input").value;
+//         let image = addGemForm.querySelector("#img-input").value;
+//         let userActivity = addGemForm.querySelector("#activity-input").value;
+//         let activities = [userActivity];
+
+//         let gem = {
+//             name,
+//             location,
+//             image,
+//             activities,
+//             likes: 0
+//         }
+//         renderGemCard(gem, gemContainer);
+
+//         fetch("http://localhost:3000/attractions", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Accept: "application/json"
+//             },
+//             body: JSON.stringify(gem)
+//         })
+//     })
+// }
+
+// function increaseLike(gem, heartsNum) {
+//     gem.likes += 1;
+//     heartsNum.textContent = gem.likes;
+
+//     fetch(`http://localhost:3000/attractions/${gem.id}`, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json"
+//         },
+//         body: JSON.stringify({likes: gem.likes})
+//     })
+// }
+
+// init()
